1a97f3fd0e273ee0f8ca9c5bc838df6175f8abd8
KhoiBao <Bronwhunte123456789@gmail.com>
2025-03-29 19:59:46 +0700

    Change from CBC to ECB for easier and secure due to not having IV

--- 3DES (full).py
+++ 3DES (full).py
@@ -1,18 +1,24 @@
 #python encryption/decryption by using DES3 - made by khoi
 from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
-import binascii
-# khởi tạo mã 24 byte cho key
-key = DES3.adjust_key_parity(get_random_bytes(24))
+from Crypto.Util.Padding import pad, unpad # "làm đầy" (padding), bổ sung thêm khi dữ liệu đưa vào không là bội số của 8, và "bỏ làm đầy" (unpadding) sau khi "làm đầy" 
+import binascii # chuyển từ chữ -> hex và hex -> chữ
+import hashlib 
 
-# IV buộc phải là 8 bytes
-iv = get_random_bytes(8)
+#Hàm xử lý key (trường hợp người dùng nhập vào không đủ 24 bytes)
+def xu_ly_key(secret_key):
+    hashed = hashlib.sha256(secret_key.encode('utf-8')).digest()
+    key = hashed[:24] 
+    # SHA-256 = Secure Hash Algorithm 256-bit chuyển đổi dữ liệu đầu vào thành chuỗi 256-bit (32 byte)
+    #digest(): tiêu hóa, tức là trả về kết quả dưới dạng byte (32 byte)
+    # [:24] chỉ lấy 24 byte đầu tiên để làm key
+    key = DES3.adjust_key_parity(key)
+    return key
 
 #Hàm mã hóa:
-def ma_hoa(message, key, iv):
+def ma_hoa(message, key):
     try:
-        cipher = DES3.new(key, DES3.MODE_CBC, iv)
+        cipher = DES3.new(key, DES3.MODE_ECB)
         goi_dem = pad(message.encode('utf-8'),DES3.block_size)
         ciphertext = cipher.encrypt(goi_dem)
         return ciphertext
@@ -21,16 +27,16 @@ def ma_hoa(message, key, iv):
         return None
     
 #Hàm Giải mã:
-def giai_ma(ciphertext,key,iv):
+def giai_ma(ciphertext,key):
     try:
-        cipher = DES3.new(key,DES3.MODE_CBC,iv)
+        cipher = DES3.new(key,DES3.MODE_ECB)
         decrypted_padded_message = cipher.decrypt(ciphertext)
         message = unpad(decrypted_padded_message,DES3.block_size)
         return message.decode('utf-8')
     except Exception as e:
         print(f"lỗi:{e}")
         return None
-   
+#============================================================== 
 #CLI
 while True:
     print("==================Menu====================")
@@ -40,33 +46,31 @@ while True:
     print("* 3-Thoát chương trình")
     choice = int(input("* Nhập lựa chọn (1-3): "))
     print("==========================================")
-
+#==============================================================
     if choice == 1:
     #input 
-        print("Nhập vào: ") 
-        message = input("")
+        message = input("Nhập vào: ")
+        secret_key = input("Nhập key: ")
     #Mã hóa nội dung có trong message
-        ciphertext = ma_hoa(message, key, iv)
-        print(f"Tin nhắn đã mã hóa:{ciphertext}")
+        key = xu_ly_key(secret_key)
+        ciphertext = ma_hoa(message, key)
         hex_ciphertext = binascii.hexlify(ciphertext).decode()
         hex_key = binascii.hexlify(key).decode()
-        hex_iv = binascii.hexlify(iv).decode()
-        # chữ -> mã hex và binascii
+        # chữ -> mã hex
+        print(f"\nTin nhắn đã mã hóa:{ciphertext}")
         print(f"Tin nhắn đã mã hóa (hex): {hex_ciphertext}")
+        print(f"\nKey:{key}")
         print(f"Key (hex): {hex_key}")
-        print(f"IV (hex): {hex_iv}") 
     
     elif choice == 2:
         hex_ciphertext = input("Nhập mã (hex) cần giải mã: ")
-        hex_key = input("Nhập key hex: ")
-        hex_iv = input("Nhập IV hex: ")
+        hex_key = input("Nhập key (hex): ")
 
         try:
             ciphertext = binascii.unhexlify(hex_ciphertext)
             key = binascii.unhexlify(hex_key)
-            iv = binascii.unhexlify(hex_iv)
             # mã hex -> chữ
-            decrypted_message = giai_ma(ciphertext, key, iv)
+            decrypted_message = giai_ma(ciphertext, key)
             print(f"\n Tin nhắn đã được giải mã: {decrypted_message}")
 
         except Exception as e:
